45d500a8825808331f60bd5e4f58bc0ffddabaf8
unknown <guillermolopez6988@yahoo.com>
2016-01-06 00:30:03 -0600

    fix and improve postfix function

--- Calculator.java
+++ Calculator.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Calculator{
-
+	
 	public static ArrayList<String> tokens;
 	public static Map<String, Integer> precedenceTable;
 	public static Stack<String> opStack; 
@@ -24,13 +24,14 @@ public class Calculator{
 		
         String postfixExpr = getPostFixExpre();
 		System.out.println("post fix expresion is: " + postfixExpr);
+		
 		tokens.clear();
 		getTokens(postfixExpr);
 
 		double result = evaluate();
 		System.out.println("Result is: " + result);
-
 	}
+
 	public static void getTokens(String expr)
 	{
 		StringTokenizer st = new StringTokenizer(expr, delimiters, true);
@@ -42,44 +43,48 @@ public class Calculator{
             	tokens.add(token);
          }
 	}
+
 	public static void populatePrecedenceTable()
 	{
 		precedenceTable.put("+", 1);
 		precedenceTable.put("-", 1);
 		precedenceTable.put("*", 2);
 		precedenceTable.put("/", 2);
+		precedenceTable.put("(", 0);
+		precedenceTable.put(")", 100);
 	}
+
 	public static double evaluate()
 	{
 		Stack<String> stack = new Stack<String>();
 
 		for(int i = 0; i < tokens.size(); i++)
 		{
-			if(!isOperator(tokens.get(i)))
-				stack.push(tokens.get(i));
-			else
+			String curToken = tokens.get(i);
+			if (isOperand(curToken))
+				stack.push(curToken);
+			else if (isOperator(curToken))
 			{
 				//if(isBinaryOperator() );
 				try{
 					double num2 = Double.parseDouble(stack.pop());
 					double num1 = Double.parseDouble(stack.pop());
-					char op = tokens.get(i).charAt(0);
+					char op = curToken.charAt(0);
 					double result = getResult(num1, num2, op);
 					stack.push(""+result);
 				}
-				catch(Exception ex)
-				{
+				catch(Exception ex){
 					System.out.println("Invalid expression");
 				}
-				
 			}
 		}
 
 		if(stack.size() == 1)
 			return Double.parseDouble(stack.pop());
 		else
-			return -1; // error
-
+			return -1;
+			//throw new ComputationException("invalid Expression");
+			//throw ComputationException("Invalid Expression");
 	}
 
 	public static double getResult(double num1, double num2, char op)
@@ -108,35 +113,32 @@ public class Calculator{
 	public static String getPostFixExpre()
 	{
 		String postfixExpr = "";
+
 		for(int i = 0; i < tokens.size(); i++)
 		{
-			if( isOperator(tokens.get(i)) )
-			{
-				if(opStack.empty() )
-					opStack.push(tokens.get(i));
-				else
-				{
-					boolean topStackPrecHigher = true;
-					boolean tokenPushed = false;
+			String curToken = tokens.get(i);
 
-					do{
-						if(precedenceTable.get(opStack.peek()) < precedenceTable.get(tokens.get(i)))
-						{
-							opStack.push(tokens.get(i));
-							topStackPrecHigher = false;
-							tokenPushed = true;
-						}
-						else //top stack higher precdecne
-							postfixExpr += " " + opStack.pop();
-							
-					}while(topStackPrecHigher && !opStack.empty());
-					
-					if(!tokenPushed)	
-						opStack.push(tokens.get(i));
-				}
+			if ( isOperand(curToken) )
+			{
+				postfixExpr += " " + curToken;
 			}
-			else
-				postfixExpr += " " + tokens.get(i);
+			else if (isOperator(curToken) )
+			{
+				while( !opStack.empty() && !hasHigherPrecedence(curToken, opStack.peek()) && !isLeftParen(opStack.peek()) )
+					postfixExpr += opStack.pop();
+
+				opStack.push(curToken);
+			}
+			else if ( isLeftParen(curToken) )
+			{
+				opStack.push(curToken);
+			}
+			else if ( isRightParen(curToken) )
+			{
+				while( !opStack.empty() && !isLeftParen(opStack.peek()) )
+					postfixExpr += " " + opStack.pop();
+				opStack.pop(); //pop the right paren
+			}
 		}
 
 		while(!opStack.empty()) //pop remaining tokens
@@ -144,13 +146,31 @@ public class Calculator{
 
 		return postfixExpr;
 	}
-	public static boolean comparePrecedence()
+
+	public static boolean isLeftParen(String str)
 	{
-		return false;
+		return (str.equals("("));
+	}
+	public static boolean isRightParen(String str)
+	{
+		return (str.equals(")")); 
+	}
+	public static boolean hasHigherPrecedence(String op1, String op2)
+	{
+		return (precedenceTable.get(op1) > precedenceTable.get(op2) );	
+	}
+	public static boolean isOperand(String str)
+	{
+		try{
+			double num = Double.parseDouble(str);
+		}
+		catch(NumberFormatException ex){
+			return false;
+		}
+		return true;
 	}
 	public static boolean isOperator(String str)
 	{
 		 return (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/") );
 	}
-
 }
